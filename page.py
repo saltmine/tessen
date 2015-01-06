a3c2124b9b8fa8bb51a5eee2d7c01072e95e88be
Ben Lewis <ben@keep.com>
2015-01-06 22:34:03 +0000

    Docs and removing file extension guessing from Page class

--- page.py
+++ page.py
@@ -21,33 +21,45 @@ class Page(object):
     self.url = page_url
     self.parsed = urlparse.urlparse(page_url)
     self.assets = []
+    self.css_files = []
     self.session = session.generate_session()
     self.response = self.session.get(page_url)
     self.soup = bs4.BeautifulSoup(self.response.content)
-    self.rewrite()
+    self.rewrite_html()
 
   @property
   def rewritten(self):
+    """HTML with asset locations rewritten
+    """
     return str(self.soup)
 
   @property
   def raw(self):
+    """Untouched HTML response
+    """
     return self.response.content
 
   def register_and_rename_asset(self, asset_url):
     """Takes an asset url, generates a new name for it (based on md5 hash)
     then adds a dictionary of the new name and url to the assets list.
+
+    We will get the file extension from the MIMETYPE
+
+    :param asset_url: can be schemaless, relative, or fully qualified.
+    :type asset_url: basestring
+    :returns: the new asset name (hash with file extension)
+    :rtype: str
     """
-    hashed = hashlib.md5(asset_url).hexdigest()
-    file_extension = asset_url.split('.')[-1]
-    name = '.'.join((hashed, file_extension))
+    name = hashlib.md5(asset_url).hexdigest()
     if asset_url.startswith('//'):
       asset_url = ''.join((self.parsed.scheme, ':', asset_url))
+    elif not asset_url.startswith('http'):
+      asset_url = ''.join((self.url, asset_url))
     self.assets.append(dict(name=name, url=asset_url))
     log.info('Renaming %s to %s', asset_url, name)
     return name
 
-  def rewrite(self):
+  def rewrite_html(self):
     """ Rewrites asset locations in the HTML, and calls
     `register_and_rename_asset` on each asset url.
     """
@@ -61,6 +73,7 @@ class Page(object):
     for link in self.soup.find_all('link'):
       if 'text/css' == link.attrs.get('type'):
         asset_url = link.attrs['href']
+        self.css_files.append(asset_url)
         link.attrs['href'] = self.register_and_rename_asset(asset_url)
 
     # Images
