068eed0587ab09d1cb557ae4a1862338591a1ad2
Ben Lewis <ben@keep.com>
2015-01-12 20:57:33 +0000

    Created Asset class for Page objects

--- page.py
+++ page.py
@@ -5,6 +5,7 @@ import bs4
 import hashlib
 import json
 import logging
+import mimetypes
 import os
 import subprocess
 import urlparse
@@ -24,15 +25,24 @@ PHANTOM_SWITCHES = ['--ssl-protocol=tlsv1', '--ignore-ssl-errors=true']
 
 
 def get_page_from_webkit(url):
+  """Call the page scraper phantomjs module with given URL.
+
+  NOTE: this function should probably be deprecated in favor of alternative
+  page scrapers.
+  """
   cmd = [PHANTOM_BIN] + PHANTOM_SWITCHES + [PHANTOM_SCRIPT, url]
   proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-  out, err = proc.communicate()
+  out, _ = proc.communicate()
   results = json.loads(out)
   return results['html'].encode('utf8')
 
 
 class Page(object):
-  """ Grabs a webpage from `page_url`, rewrites static asset routes
+  """ Grabs a webpage from `page_url`, provides interface to download and
+  rewrite static assets.
+
+  The `Page` class scans through an HTML document, registering static assets
+  under `Page.assets`.
   """
 
   def __init__(self, page_url, html=None):
@@ -61,26 +71,28 @@ class Page(object):
     """
     return self._html
 
-  def register_and_rename_asset(self, asset_url, default_file_extension=None):
-    """Takes an asset url, generates a new name for it (based on md5 hash)
-    then adds a dictionary of the new name and url to the assets list.
+  def register_asset(self, asset, url_attr, default_file_extension=None):
+    """Takes an asset (BeautifulSoup node) discovers it's absolute url (if a
+    scheme-less or relative URL is provided), and creates an `Asset` instance
+    and appends to self.assets.
 
-    We will get the file extension from the MIMETYPE
-
-    :param asset_url: can be schemaless, relative, or fully qualified.
-    :type asset_url: basestring
-    :returns: the new asset name (hash with file extension)
-    :rtype: str
+    :param asset: BeautifulSoup node
+    :param url_attr: the name of the HTML attribute that holds the asset name
+                     location. (e.g., 'href' or 'src'). Allows us to rewrite
+                     the url later on
+    :param default_file_extension: the fall back file extension in case one
+                                   can't be inferred.
     """
-    name = hashlib.md5(asset_url).hexdigest()
+    # If the URL is scheme-less or relative, rewrite as fully qualified. This
+    # needs to happen here so we can use the parsed url of the base page.
+    asset_url = asset.attrs[url_attr]
     if asset_url.startswith('//'): # Scheme less
-      asset_url = ''.join((self.parsed.scheme, ':', asset_url))
+      asset_url = ':'.join((self.parsed.scheme, asset_url))
     elif not asset_url.startswith('http'): # Relative
       asset_url = urlparse.urljoin(self.url, asset_url)
-    self.assets.append(dict(name=name, url=asset_url,
-        default_file_extension=default_file_extension))
-    log.info('Renaming "%s" to "%s"', asset_url, name)
-    return name
+
+    _asset = Asset(asset, asset_url, url_attr, default_file_extension)
+    self.assets.append(_asset)
 
   def rewrite_html(self):
     """ Rewrites asset locations in the HTML, and calls
@@ -88,22 +100,18 @@ class Page(object):
     """
     # JS
     for script in self.soup.find_all('script'):
-      if 'src' in script.attrs:
-        asset_url = script.attrs['src']
-        script.attrs['src'] = self.register_and_rename_asset(asset_url, '.js')
+      if script.attrs.get('src'):
+        script.attrs['src'] = self.register_asset(script, 'src', '.js')
 
     # CSS, etc
     for link in self.soup.find_all('link'):
       if link.attrs.get('href'):
-        asset_url = link.attrs['href']
-        link.attrs['href'] = self.register_and_rename_asset(asset_url, '.css')
+        link.attrs['href'] = self.register_asset(link, 'href', '.css')
 
     # Iframes
     for iframe in self.soup.find_all('iframe'):
-      if 'src' in iframe.attrs:
-        asset_url = iframe.attrs['src']
-        iframe.attrs['src'] = self.register_and_rename_asset(asset_url,
-            '.html')
+      if iframe.attrs.get('src'):
+        iframe.attrs['src'] = self.register_asset(iframe, 'src', '.html')
 
     # Images
     for img in self.soup.find_all('img'):
@@ -114,5 +122,84 @@ class Page(object):
         attr_name = None
       if attr_name is None:
         continue
-      asset_url = img.attrs[attr_name]
-      img.attrs[attr_name] = self.register_and_rename_asset(asset_url, '.jpeg')
+      self.register_asset(img, attr_name, '.jpeg')
+
+
+class Asset(object):
+  """Wraps the beautifulsoup (asset) node object providing an interface to
+  download its content and update its url.
+
+  We infer the file extension based on Content Type header returned from the
+  HTTP request. The Asset object provides a proxy for updating the HTML page.
+  """
+  def __init__(self, asset, asset_url, url_attr, default_file_extension=None):
+    # Note: even though we can get the asset url from the asset, it is often
+    # scheme-less or relative. Therefore, we expect the `asset_url` param to be
+    # fully qualified.
+    self._asset = asset
+    self._url_attr = url_attr
+    self._response = None
+    self._file_extension = None
+    self._default_file_extension = default_file_extension
+    self.name = None
+    self.hash = hashlib.md5(self._asset[self._url_attr]).hexdigest()
+    self.asset_url = asset_url
+
+  @property
+  def content(self):
+    """ Return the request content (to save to file)
+    """
+    return self._response.content
+
+  def download(self, session_):
+    """Requests and stores asset. We pass in the session object explicitly here
+    in case we need to modify headers/cookies later on in the storage cycle.
+    """
+    self._response = session_.get(self.asset_url)
+
+  def _get_file_extension(self):
+    """Determine file extension in the following precedence order:
+        (1) inspecting content type header,
+        (2) inspecting the file extension in the URL, or
+        (3) using the optional default file extension.
+    """
+    content_string = self._response.headers.get('content-type', '')
+    if content_string:
+      # Some sites return "content/type; encoding info". Isolate content type
+      content_type = content_string.split(';')[0]
+      file_extension = mimetypes.guess_extension(content_type)
+    else:
+      # Fall back on filename extensions in URL
+      uri = urlparse.urlparse(self.asset_url).path
+      if '.' in uri:
+        file_extension = uri.split('.')[-1]
+        if len(file_extension) <= 10:
+          # Make sure file extension isn't too long, add the leading period
+          file_extension = '.%s' % file_extension
+          log.info('Content type is empty, found "%s" in url', file_extension)
+        else:
+          # too long, unset
+          file_extension = None
+
+      # Check that we have a file extension
+      if not file_extension and self._default_file_extension:
+        log.info('Using default file extnsion "%s"',
+                 self._default_file_extension)
+        file_extension = self._default_file_extension
+      elif not file_extension:
+        log.warn('Could not determine file extension for asset "%s", '
+                 'content_type "%s"', self.asset_url, content_string)
+
+    self._file_extension = file_extension
+
+  def rename(self):
+    """Rename the static asset. Should only be run _after_ `self.download` is
+    called. If file extension exists, join it to the hash, otherwise, rename
+    asset with hash value only.
+    """
+    self._get_file_extension()
+    if self._file_extension:
+      self.name = ''.join((self.hash, self._file_extension))
+    else:
+      self.name = self.hash
+    self._asset[self._url_attr] = self.name
--- storage/helpers.py
+++ storage/helpers.py
@@ -23,45 +23,15 @@ log = logging.getLogger(__name__)
 def store_page(page):
   """ Takes a `page.Page` object and stores the rewritten static assets
   """
-  # First write the page.
-  storage.store_file('raw.html', page.raw)
-  storage.store_file('index.html', page.rewritten)
+  # First write the assets
 
   for asset in page.assets:
-    res = page.session.get(asset['url'])
-    # TODO: Error handler on request, break out file extension into separate
-    # function
-    content_string = res.headers.get('content-type', '')
-    if content_string:
-      # Some sites return "content/type; encoding info". Isolate content type
-      content_type = content_string.split(';')[0]
-      file_extension = mimetypes.guess_extension(content_type)
-    else:
-      # Fall back on filename extensions in URL
-      uri = urlparse.urlparse(asset['url']).path
-      if '.' in uri:
-        file_extension = uri.split('.')[-1]
-        if len(file_extension) <= 10:
-          # Make sure file extension isn't too long, add the leading period
-          file_extension = '.%s' % file_extension
-          log.warn('Content type is empty, found "%s" in url', file_extension)
-        else:
-          # too long, unset
-          file_extension = None
+    asset.download(page.session)
+    asset.rename()
+    storage.store_file(asset.name, asset.content)
 
-    # Check that we have a file extension
-    default_file_extension = asset['default_file_extension']
-    if not file_extension and default_file_extension:
-      log.warn('Using default file extnsion "%s"', default_file_extension)
-      file_extension = default_file_extension
-    elif not file_extension:
-      log.warn('Could not determine file extension for asset "%s", '
-          'content_type "%s"', asset['url'], content_string)
-      continue
-    name = ''.join((asset['name'], file_extension))
-
-    storage.store_file(name, res.content)
-
+  storage.store_file('raw.html', page.raw)
+  storage.store_file('index.html', page.rewritten)
   _store_hash_map(page.assets)
 
 
@@ -69,7 +39,7 @@ def _store_hash_map(assets, name=None):
   """
   """
   data = ['Hashed name, Original Asset URL']
-  data.extend(['%s, %s' % (a['name'], a['url']) for a in assets])
+  data.extend(['%s, %s' % (a.name, a.asset_url) for a in assets])
   if name is None:
     name = 'hashmap.txt'
   storage.store_file(name, '\n'.join(data))
