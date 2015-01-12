eb8285f0e5f08cddb67e07a1b144fe2aa85d60ce
Ben Lewis <ben@keep.com>
2015-01-12 21:04:16 +0000

    Cleaned up crawl script

--- crawl.py
+++ crawl.py
@@ -2,21 +2,24 @@
 # -*- coding: utf-8 -*-
 """ Like this and like that and like this and uh
 """
+import argparse
 import logging
 import requests_cache
 
-from page import Page
+import page
 from storage import store_page
 
 
 log = logging.getLogger(__name__)
 requests_cache.install_cache()
-URL = 'https://www.jcrew.com/womens_feature/NewArrivals/PRD~B8448/B8448.jsp?intcmp=cathead_w_newarrivals'
-URL = 'http://google.com'
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('url', metavar='url', type=str, nargs='+',
+                    help='The URL to crawl and store')
 
 
 if __name__ == '__main__':
   logging.basicConfig()
-  page = Page(URL)
-  import IPython; IPython.embed()
+  url = parser.parse_args().url[0]
+  html = page.get_page_from_webkit(url)
+  page = page.Page(url, html=html)
   store_page(page)
