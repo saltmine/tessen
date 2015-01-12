cbd26c5c251870665251172f8a2cd02d80d73eeb
Ben Lewis <ben@keep.com>
2015-01-12 16:08:36 +0000

    Adding better extension handling

--- storage/helpers.py
+++ storage/helpers.py
@@ -4,6 +4,7 @@ we should probably use some type of threading.
 """
 import logging
 import mimetypes
+import urlparse
 
 from .backends import storage
 
@@ -11,6 +12,7 @@ from .backends import storage
 ADDITIONAL_TYPES = (('text/javascript', '.js'),)
 
 
+# Add additional, non-standard mime type maps here.
 for mime, extension in ADDITIONAL_TYPES:
   mimetypes.add_type(mime, extension)
 
@@ -28,12 +30,22 @@ def store_page(page):
   for asset in page.assets:
     res = page.session.get(asset['url'])
     # TODO: Error handler
-    content_type = res.headers.get('content-type', '').split(';')[0]
-    file_extension = mimetypes.guess_extension(content_type)
+    content_string = res.headers.get('content-type', '')
+    if content_string:
+      # Some sites return "content/type; encoding info". Isolate content type
+      content_type = content_string.split(';')[0]
+      file_extension = mimetypes.guess_extension(content_type)
+    else:
+      # Fall back on filename extensions
+      uri = urlparse.urlparse(asset['url']).path
+      file_extension = uri.split('.')[-1]
+      name = ''.join((asset['name'], file_extension))
+      log.warn('Content type is empty, found "%s" in url', file_extension)
+
     if file_extension:
       name = ''.join((asset['name'], file_extension))
     else:
-      log.warn('Content type %s did not return an extension', content_type)
+      log.warn('Content type "%s" did not return an extension', content_string)
       continue
     storage.store_file(name, res.content)
 
